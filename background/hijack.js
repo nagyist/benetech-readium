3bae48a2bdbaa1d7b8e80a3eaccafa1c60599c52
Martin Quiazon <martinq@benetech.org>
2012-09-17 15:21:29 -0700

    Initial patching of CRX manifest and URl intercept

---
+++ background/hijack.js
@@ -0,0 +1,9 @@
+console.log(window.location.href);
+var matches = /(?:http|https)\:\/\/reader(\.\w*?){0,1}\.bookshare\.org\/viewer\.html\?book=(\d*)/.exec(window.location.href);
+console.log(matches);
+if (matches != null) {
+	var env = matches[1];
+	var titleId = matches[2];
+	alert(chrome.extension.getURL('views/viewer.html') + '?title=' + titleId);
+	window.location.href = chrome.extension.getURL('views/viewer.html') + '?book=' + titleId;
+}
--- scripts/routers/crx_viewer_router.js
+++ scripts/routers/crx_viewer_router.js
@@ -1,3 +1,4 @@
+// This is the router used by the web served version of readium
 // This router is used in the book view of the chrome extension build of readium
 Readium.Routers.ViewerRouter = Backbone.Router.extend({
 
@@ -8,39 +9,41 @@ Readium.Routers.ViewerRouter = Backbone.Router.extend({
 
 	openBook: function(key) {
 
-		var ePUBViewPrefKey = key + "_epubViewProperties";
-
-		// Get the saved ePUB and the ePUB's view preferences
-		Lawnchair(function() {
-
-			// Get saved ePUB
-			this.get(key, function(result) {
-				
-				if(result === null) {
-					
-					alert('Could not load ePUB, try refeshing your browser.');
-					return;
-				}
-
-				window._epub = new Readium.Models.EPUB(result);
-
-				// TODO: The structure of this method is potentially problematic: A lawnchair call nested in another.
-				//   Investigate changes and asynchronicity. 
-				// Get saved ePUB view preferences, if they exist
-				this.get(ePUBViewPrefKey, function(result) {
-				
-					// Instantiate the ePUB controller 
-					window._epubController = new Readium.Models.EPUBController(_.extend({epub : window._epub}, result));
-					window._applicationView = new Readium.Views.ViewerApplicationView({
-								model: window._epubController
-							});
-					window._applicationView.render();
-				});
-			});
+		// Ask the server for the book's data
+		var self = this;
+		$.ajax({
+			url: 'http://martinq-laptop.local:9000/bookInfo?title=' + key,
+			dataType: 'json',
+			success: function(data, textStatus, jqXHR) {
+				console.log(data);
+				self.initViewer(data);
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log(errorThrown);
+				self.initViewer(null);
+			}
 		});
+	},
+
+	initViewer: function(book_data) {
+		if(book_data) {
+			// initialize the viewer for that book
+			window._epub = new Readium.Models.EPUB(book_data);
+			window._epubController = new Readium.Models.EPUBController(_.extend({epub : window._epub}, book_data));
+			window._applicationView = new Readium.Views.ViewerApplicationView({
+				model: window._epubController
+			});
+			window._applicationView.render();
+		}
+		else {
+			// did not find the book in our library
+			alert("The book you requested does not exist");
+		}
+
 	},
 
 	splat_handler: function(splat) {
 		console.log(splat)
 	}
+
 });
