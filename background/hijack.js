2f80bbfc984ee175aeb16a451e5a16d56977be40
Martin Quiazon <martinq@benetech.org>
2012-09-17 16:51:17 -0700

    More removals; bye bye library link

--- background/hijack.js
+++ background/hijack.js
@@ -1,6 +1,4 @@
-console.log(window.location.href);
 var matches = /(?:http|https)\:\/\/reader(\.\w*?){0,1}\.bookshare\.org\/viewer\.html\?book=(.*)/.exec(window.location.href);
-console.log(matches);
 if (matches != null) {
 	var env = matches[1];
 	var titleId = matches[2];
