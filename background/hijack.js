984eeec5ad580657edf6bb3d67a6902afaf0833e
Martin Quiazon <martinq@benetech.org>
2012-09-17 16:46:13 -0700

    Nuking all unnecessary extension code

--- background/hijack.js
+++ background/hijack.js
@@ -1,5 +1,5 @@
 console.log(window.location.href);
-var matches = /(?:http|https)\:\/\/reader(\.\w*?){0,1}\.bookshare\.org\/viewer\.html\?book=(\d*)/.exec(window.location.href);
+var matches = /(?:http|https)\:\/\/reader(\.\w*?){0,1}\.bookshare\.org\/viewer\.html\?book=(.*)/.exec(window.location.href);
 console.log(matches);
 if (matches != null) {
 	var env = matches[1];
