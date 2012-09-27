6cd07a573ee71598f345b56d6b8a6430077cf818
Martin Quiazon <martinq@benetech.org>
2012-09-26 21:44:42 -0700

    BKS-21638 Inline chrome installation

--- scripts/namespaces/crx_viewer_namespace.js
+++ scripts/namespaces/crx_viewer_namespace.js
@@ -8,19 +8,43 @@ window.Readium = {
 	Routers: {},
 	Utils: {},
 	Init: function() {
-		_router = new Readium.Routers.ViewerRouter();
+		if (window.chrome && window.chrome.extension) {
+			BookshareUtils.setEnvironment(window.document.referrer);
+		} else {
+			BookshareUtils.setEnvironment(window.location.href);
+			$('#bar-logo').attr('href', BookshareUtils.resolveEnvironment('https://www.bookshare.org/'));
+			_router = new Readium.Routers.ViewerRouter();
+
+			if (window.chrome && !window.chrome.app.isInstalled) {
+				var options = Readium.Models.ReadiumOptions.getInstance();
+				if (false == !!options.get('decline_extension')) {
+					$('#install-extension-but').click(
+						function(e) {
+							chrome.webstore.install();
+						});
+					$('#no-thanks-but').click(
+						function(e) {
+							options.set('decline_extension', true);
+							options.save();
+							$('#chrome-extension-install').modal('hide');
+							window.Readium.Start();
+						});
+					$('#chrome-extension-install').modal();
+				} else {
+					this.Start();
+				}
+			} else {
+				this.Start();
+			}
+		}
+	},
+
+	Start: function() {
 		Backbone.history.start({pushState: true});
 	}
 };
 
 $(function() {
 	// call the initialization code when the dom is loaded
-	if (window.chrome && window.chrome.extension) {
-		BookshareUtils.setEnvironment(window.document.referrer);
-	} else {
-		BookshareUtils.setEnvironment(window.location.href);
-	}
-
-	$('#bar-logo').attr('href', BookshareUtils.resolveEnvironment('https://www.bookshare.org/'));
 	window.Readium.Init();
 });
