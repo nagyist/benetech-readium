6a5bc448e422871999beab22f7a06661960a1df6
Martin Quiazon <martinq@benetech.org>
2012-12-13 16:17:42 -0800

    READ-7 First stab at pulling in Readium changes.

--- scripts/views/toc_view.js
+++ scripts/views/toc_view.js
@@ -9,8 +9,7 @@ Readium.Views.TocViewBase = Backbone.View.extend({
 
 	events: {
 		"click a": "handleClick",
-		"click #close-toc-button": "closeToc",
-		"keyup #close-toc-button": "closeToc"
+		"click #close-toc-button": "closeToc"
 	},
 
 	setVisibility: function() {
@@ -21,38 +20,18 @@ Readium.Views.TocViewBase = Backbone.View.extend({
 		e.preventDefault();
 		href = $(e.currentTarget).attr("href");
 		this.model.handleLink(href);
-		var splitUrl = BookshareUtils.getSplitUrl(href);
+	},
 
-		// handle the base url first:
-		if(splitUrl[1]) {
-			this.setFocus(splitUrl[2]);
-		}
+	handleSelect : function (e) {
+
+		var href = e.val;
+		this.model.handleLink(href);
 	},
 
 	closeToc: function(e) {
-		if (e.type == "click" || (e.type == "keyup" && e.keyCode == 13)) {
-			e.preventDefault();
-			this.model.hide();
-		}
-	}, 
-
-	setFocus: function(goToId) {
-		var contentsFrame = $(window._epubController.paginator.v.getFrame());
-		contentsFrame.focus();
-		var elementToFocusOn = contentsFrame.contents().find("#"+goToId);
-		if ($(elementToFocusOn).length > 0) {
-			setTimeout(function() {
-				elementToFocusOn.attr('tabindex', '-1').focus();
-			}, 500);
-		}
-	},
-
-	scrollToNavItem: function(el) {
-		var yPos = el.getClientRects()[0].top;
-		var tocBody = this.$('#toc-body');
-		tocBody.scrollTop(tocBody.scrollTop() + yPos - Math.ceil(tocBody.height() * 0.4));
+		e.preventDefault();
+		this.model.hide();
 	}
-
 });
 
 
@@ -64,36 +43,107 @@ Readium.Views.NcxTocView = Readium.Views.TocViewBase.extend({
 	},
 
 	render: function() {
+
+		var ol;
+
 		this.setVisibility();
-		var ol = $("<ol></ol>");
-		var navs = this.model.get("navs");
-		for(var i = 0; i < navs.length; i++) {
-			ol.append( this.nav_template(navs[i]) );
-		}
-		this.$('#toc-body').html("<h1 tabindex='-1' id='toc-heading-ref'>" + (this.model.get("title") || "Table of Contents") + "</h1>");
+
+		// Construct html for the navPoints in the document		
+		ol = this.addNavPointElements(this.model.get("navs"));
+
+		this.$('#toc-body').html("<h2 tabindex='-1'>" + (this.model.get("title") || "Contents") + "</h2>")
 		this.$('#toc-body').append(ol);
 		this.$('#toc-body').append("<div id='toc-end-spacer'>");
 		return this;
-	}
+	},
 
+	// Description: Constructs an html representation of NCX navPoints, based on an object of navPoint information
+	// Rationale: This is a recursive method, as NCX navPoint elements can nest 0 or more of themselves as children
+	addNavPointElements: function (jsonNavs) {
+
+		var ol = $("<ol></ol>");
+		var that = this;
+
+		$.each(jsonNavs, function (navIndex) {
+
+			var hasNavs;
+
+			// Add the current navPoint element to the TOC html 
+			ol.append( that.nav_template(jsonNavs[navIndex]) );
+
+			// Check if the current navPoint has navPoints of its own
+			hasNavs = jsonNavs[navIndex].navs.length > 0 ? true : false;
+			if (hasNavs) {
+
+				var li = $("<li></li>");
+				li.append(that.addNavPointElements(jsonNavs[navIndex].navs));
+				ol.append(li);
+			}
+		});
+
+		return ol; 
+	}
 });
 
 Readium.Views.XhtmlTocView = Readium.Views.TocViewBase.extend({ 
 
+	// ------------------------------------------------------------------------------------ //
+	//  "PUBLIC" METHODS (THE API)                                                          //
+	// ------------------------------------------------------------------------------------ //
+
 	render: function() {
+			
 		this.$('#toc-body').html( this.model.get("body").html() );
+		this.formatPageListNavigation();
 		this.$('#toc-body').append("<div id='toc-end-spacer'>");
-		if (this.model.get("visible")) {
-			this.renderTocHighlight();
-		}
 		return this;
 	},
 
-	renderTocHighlight: function() {
-		var selector = this.model.get("toc_highlight_selector");
-		if (selector) {
-			var targetLink = this.$('#toc-body').find(selector).addClass("tocHighlight");
-			this.scrollToNavItem(targetLink[0]);
-		}
+	// ------------------------------------------------------------------------------------ //
+	//  "PRIVATE" HELPERS                                                                   //
+	// ------------------------------------------------------------------------------------ //
+
+	formatPageListNavigation : function () {
+
+		var $navElements;
+		var $pageListNavElement;
+		var pageListData = [];
+
+		// Search for a nav element with epub:type="page-list". A nav element of this type must not occur more than once.
+		$navElements = this.$("nav");
+		$pageListNavElement = $navElements.filter(function () {
+
+			if ($(this).attr("epub:type") === 'page-list') {
+
+				// Hide the standard XHTML page-list nav element, as we'll be displaying a select2 drop-down control for this.
+				$(this).attr("id", "page-list-select");
+				$(this).hide();
+				return true;
+			}
+		});
+
+		// Each nav element has a single ordered list of page numbers. Extract this data into an array so it can be 
+		//   loaded in the page-list control
+		// TODO: span elements can be used to create sub-headings. Implement functionality to account for this at some point.
+		$.each($('a', $pageListNavElement), function () { 
+
+			var $navTarget = $(this);
+			pageListData.push({
+
+				id : $navTarget.attr("href"),
+				text : "Page " + $navTarget.text()
+			});
+		});
+
+		// Create the select2 control
+		$("#page-list-select").select2({
+
+			placeholder : "Select a page",
+			data : pageListData
+		});
+
+		// the select2 adds no-op inline click handlers, but these are not allowed
+		// by chromes content securty policy so just remove them
+		this.$('[onclick]').removeAttr('onclick');
 	}
 });
