79560121fd20972cfe8e87577a8d2f945cabbc12
Martin Quiazon <martinq@benetech.org>
2013-01-08 10:50:09 -0800

    READ-12 backbone history removal

--- scripts/namespaces/crx_viewer_namespace.js
+++ scripts/namespaces/crx_viewer_namespace.js
@@ -61,8 +61,42 @@ window.Readium = {
 	},
 
 	Start: function() {
-		Backbone.history.start({pushState: true});
-	}
+		// Detect whether IE is the current browser, and use an alternative to HTML-5 push state and backbone.js routing
+		if (this.isIE9()) {
+
+			// Get the book id
+			var queryString = window.location.search;
+			var firstQueryParam = queryString.split("&")[0];
+			var epubId = firstQueryParam.split("=")[1];
+
+			// Open the book with the method that the backbone router would have called. 
+			_router.openBook(epubId);
+		}
+		// Assumes HTML-5 compatible browser: Safari, Firefox, Chrome, IE10
+		else {
+			Backbone.history.start({pushState: true});
+		}
+	},
+
+	isIE9 : function() {
+
+	    var undef;
+	    var v = 3;
+	    var div = document.createElement('div');
+	    var all = div.getElementsByTagName('i');
+
+	    while (
+	        div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+	        all[0]
+	    );
+
+	    if (v === 9) {
+	    	return true;
+	    }
+	    else {
+	    	return false;
+	    }
+	}	
 };
 
 $(function() {
