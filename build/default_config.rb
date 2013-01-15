3ca77db898315bfb8be3ffbed1df0040ce8176d7
Martin Quiazon <martinq@benetech.org>
2013-01-15 14:14:57 -0800

    Removed select2 files.

--- build/default_config.rb
+++ build/default_config.rb
@@ -27,7 +27,7 @@
 @config[:simple_copies] = ["background/**/*", "css/viewer_manifest.css", "css/library.css", "images/**/*", "manifest.json", "LICENSE", ]
 
 # list of js libraries that need to be copied over (right now these are just simple copies)
-@config[:js_libs] = ["lib/jquery-1.8.3.min.js", "lib/mathjax/**/*", "lib/pan_and_zoom.js", "scripts/libs/plugins.js", "scripts/libs/web_plugins.js", "lib/modernizr-2.5.3.min.js", "lib/2.5.3-crypto-sha1.js", "lib/select2.min.js", "lib/images/*"]
+@config[:js_libs] = ["lib/jquery-1.8.3.min.js", "lib/mathjax/**/*", "lib/pan_and_zoom.js", "scripts/libs/plugins.js", "scripts/libs/web_plugins.js", "lib/modernizr-2.5.3.min.js", "lib/2.5.3-crypto-sha1.js", "lib/images/*"]
 
 # html view files that need to have be processed (scripts) and copied over
 @config[:html_files] = ["views/viewer.html", "views/page.xhtml", "viewer.html", "page.xhtml"]
