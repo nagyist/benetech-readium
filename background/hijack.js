d718563c7b18bb91f94fe3b1e77842bc5f39948b
Martin Quiazon <martinq@benetech.org>
2013-10-25 17:02:32 -0700

    READ-179 Updated manifest.json to handle stricter linking policy in Chrome 31

--- background/hijack.js
+++ background/hijack.js
@@ -2,5 +2,9 @@ var matches = /https\:\/\/(?:(qa|staging)\-){0,1}bookshare\-reader\.s3\.amazonaw
 if (matches != null) {
 	var env = matches[1];
 	var titleId = matches[2];
-	window.location.href = chrome.extension.getURL('views/viewer.html') + '?book=' + titleId + '&env=' + env;
+	var url = chrome.extension.getURL('views/viewer.html') + '?book=' + titleId;
+	if (env != undefined) {
+		url = url + "&env=" + env;
+	}
+	window.location.href = url;
 }
