6615321c6ebce5ff06978f5c811bb5a9e9dac76a
Paul Anderson <paul.anderson@benetech.org>
2015-03-27 16:19:54 -0700

    READ-240 track website vs extension on analytics

--- scripts/utils/google_analytics_loader.js
+++ scripts/utils/google_analytics_loader.js
@@ -1,6 +1,9 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-2390830-17']);
+var _readerApp = location.protocol == "chrome-extension:" ? ("extension-"+chrome.app.getDetails().version) : "website";
+_gaq.push(['_setCustomVar', 2, 'ReaderApp', _readerApp, 2]);
 _gaq.push(['_trackPageview']);
+
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = 'https://ssl.google-analytics.com/ga.js';
