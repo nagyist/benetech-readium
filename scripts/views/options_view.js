ee65d3b252249a6fdd5e0fc71c5b1879107e1114
Jason Schwab <jasons@benetech.org>
2016-08-10 17:32:35 -0700

    read-301

--- scripts/views/options_view.js
+++ scripts/views/options_view.js
@@ -85,18 +85,34 @@ Readium.Views.OptionsView = Backbone.View.extend({
 	},
 	
 	renderBeelineNotification: function() {
-		alert("Something");
-		
-		//var vis = false;
-		//var beeline = this.model.get("controller").get("beeline");
-		//console.log("renderBeelineMenu: " + beeline);
-		//if( beeline && beeline == 'on') {
-		//	vis = true;
-		//}
-		// Only display if beta option was selected
-		//this.$("#beeline-beta-menu").toggle(vis);
-		//this.$("#beeline-theme-option").toggle(vis);
-		//this.renderBeelineOption();
+		function createCookie(name,value,days) {
+		    if (days) {
+		        var date = new Date();
+		        date.setTime(date.getTime()+(days*24*60*60*1000));
+		        var expires = "; expires="+date.toGMTString();
+		    }
+		    else var expires = "";
+		    document.cookie = name+"="+value+expires+"; path=/";
+		}
+
+		function readCookie(name) {
+		    var nameEQ = name + "=";
+		    var ca = document.cookie.split(';');
+		    for(var i=0;i < ca.length;i++) {
+		        var c = ca[i];
+		        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		    }
+		    return null;
+		}
+		var beeline = this.model.get("controller").get("beeline");
+		console.log("renderBeelineNotification: " + beeline);
+		if( beeline && beeline == 'on') {
+			var x = readCookie('new-feature-beeline')
+			if (x=='show'||x==null) {
+			    $('#new-feature-beeline-modal').modal('show');
+			}
+		}
 	},
 		
 	render: function() {
