de2afa933cf92c8c4a6afb4d9c7263657b6db43c
Jason Schwab <jasons@benetech.org>
2016-08-31 01:18:01 -0700

    READ301: trial in the beeline view js file

--- scripts/views/beeline_notification_view.js
+++ scripts/views/beeline_notification_view.js
@@ -3,44 +3,34 @@ Readium.Views.BeelineNotificationView = Backbone.View.extend({
 	el: '#beeline-notification-modal',
 
 	initialize: function() {
-		//
+		// if cookie is not set
 		$('#beeline-notification-modal').modal('show');
-
-		//$('#help-btn').attr('aria-pressed', 'false');
-		//$('#viewer-help-modal').on('shown', function(){
-		//	$('#help-heading').focus();
-		//	setTimeout(function(){
-		//		$('#help-btn').attr('aria-pressed', 'true');
-		//	}, 1);
-		//}).on('hidden', function(){
-		//	setTimeout(function(){
-		//		$('#help-btn').attr('aria-pressed', 'false').focus();
-		//	}, 1);
-		//});
+		//else do nothing
 	},
 
 	events: {
-    	"click #beeline-notification-button-yes": 	"BeelineYes",
-    	"click #beeline-notification-button-no": 	"BeelineNo",
+    	//"click #beeline-notification-button-yes": 	"BeelineYes",
+    	//"click #beeline-notification-button-no": 	"BeelineNo",
   	},
 
 	render: function() {
 		return this;
 	},
   	
-  	BeelineYes: function(e) {
-  		this.$el.modal('hide');
-  		alert("YES, you clicked the Show Beeline Button");
+  	//BeelineYes: function(e) {
+  		//this.$el.modal('hide');
+  		//alert("YES, you clicked the Show Beeline Button");
   		//set cookie to doNotShow
   		//open settings modal with Beeline selected.
 		
-  	},
+  	//},
   	
-  	BeelineNo: function(e) {
-  		this.$el.modal('hide');
-  		alert("No, you do not want to try beeline");
+  	//BeelineNo: function(e) {
+  		//this.$el.modal('hide');
+  		//alert("No, you do not want to try beeline");
   		//set cookie to doNotShow
 
+  		//DOES THIS RETURN FOCUS FOR SCREEN READERS?
   		//$('#help-btn').focus();
-  	}
+  	//}
 });
