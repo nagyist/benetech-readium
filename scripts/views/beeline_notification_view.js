5a09dc317529127f9189530ef0e65aee99524402
Jason Schwab <jasons@benetech.org>
2016-09-01 01:14:31 -0700

    READ301

--- scripts/views/beeline_notification_view.js
+++ scripts/views/beeline_notification_view.js
@@ -43,18 +43,7 @@ Readium.Views.BeelineNotificationView = Backbone.View.extend({
 	el: '#viewer-help-modal',
 
 	initialize: function() {
-		console.log("Initializing Help Modal(new).");
-		$('#help-btn').attr('aria-pressed', 'false');
-		$('#viewer-help-modal').on('shown', function(){
-			$('#help-heading').focus();
-			setTimeout(function(){
-				$('#help-btn').attr('aria-pressed', 'true');
-			}, 1);
-		}).on('hidden', function(){
-			setTimeout(function(){
-				$('#help-btn').attr('aria-pressed', 'false').focus();
-			}, 1);
-		});
+		console.log("Initializing Help Modal(new1).");
 	},
 
 	events: {
